5ab273573a54e0152d24661c8aa4bd588a3780e6
Jun Liang <illuminatemynight@gmail.com>
2019-08-04 23:50:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence1)
   array_sentence1 = sentence1.split(" ")
   puts array_sentence1
   new_array = []
-  array_sentence1.each do |words| 
+  array_sentence1.each do |words|
     new_array << "#{words.reverse}"
-  end 
+  end
   return new_array
 end 
